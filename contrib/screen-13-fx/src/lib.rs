8bd0dc350873ac51e1d1b0f43129701947d04dce
John Wells <john@attackgoat.com>
2022-02-11 10:42:18 -0500

    Add prelude
    (cherry picked from commit e122057b633b2188425ac346a046f553fa9f8e5a)

--- contrib/screen-13-fx/src/lib.rs
+++ contrib/screen-13-fx/src/lib.rs
@@ -1,3 +1,23 @@
+pub mod prelude_arc {
+    pub use super::*;
+
+    use screen_13::ptr::ArcK as P;
+
+    pub type ComputePresenter = super::ComputePresenter<P>;
+    pub type GraphicPresenter = super::GraphicPresenter<P>;
+    pub type ImageLoader = super::ImageLoader<P>;
+}
+
+pub mod prelude_rc {
+    pub use super::*;
+
+    use screen_13::ptr::RcK as P;
+
+    pub type ComputePresenter = super::ComputePresenter<P>;
+    pub type GraphicPresenter = super::GraphicPresenter<P>;
+    pub type ImageLoader = super::ImageLoader<P>;
+}
+
 mod res {
     pub mod shader {
         include!(concat!(env!("OUT_DIR"), "/shader_bindings.rs"));
