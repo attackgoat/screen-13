19ebea2e278fce9dec9d22eccb7d6e0ab039c59f
Niklas Voss <niklas.voss@gmail.com>
2023-01-25 20:25:04 +0100

    Add comment to explain ash re-export by macro crate

--- contrib/screen-13-macros/src/lib.rs
+++ contrib/screen-13-macros/src/lib.rs
@@ -6,10 +6,12 @@
 // https://github.com/bkchr/proc-macro-crate/issues/10
 #[allow(unused_extern_crates)]
 extern crate self as screen_13_macros;
-
-pub mod vertex;
+// We re-export ash to avoid having to access screen_13 namespace from macros.
+// The reason is to avoid the issues referenced above and contain the workaround
+// to this crate.
 pub use screen_13::driver::ash;
 
+pub mod vertex;
 pub mod prelude {
     pub use super::vertex::{ShaderBuilderExt, Vertex};
 }
